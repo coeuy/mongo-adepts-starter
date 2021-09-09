d6ad80021e5375df03aa395368e661d54e0706f9
nalhoug <Me914374385>
2021-09-09 10:07:39 +0800

    Lambada构造器

---
+++ src/main/java/com/coeuy/osp/mongo/adepts/model/query/LambdaQueryAdepts.java
@@ -0,0 +1,12 @@
+package com.coeuy.osp.mongo.adepts.model.query;
+
+/**
+ * <p>
+ * Explain
+ * </p>
+ *
+ * @author yarnk
+ * @date 2021/9/9
+ */
+public class LambdaQueryAdepts extends QueryAdepts{
+}
